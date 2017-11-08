a6273736395ac6fca1ab225bd092deeb4554bbdb
elizabethmg <31739691+elizabethmg@users.noreply.github.com>
2017-11-08 16:23:16 -0500

    Create pj1login.php

---
+++ pj1login.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="signuphwstyle.css">
+	<title>Login</title>
+</head>
+
+<body>
+ <h1 align="center">Welcome to the sign up page!</h1>
+
+<p align="center">Please fill out the page bellow</p>
+
+<div align="center">
+    <form ><br>
+    <p>Your username is the email used from the sign up page.</p>
+    <input type="text" id="email" placeholder="Username" required><br><br>
+
+    <input type="text" id="password" placeholder="Password" required><br><br>
+    <input type="submit" value="Log In"/></a>
+
+</div> 
+</body>
+
+</html>
