ec32778b17d548e19d2fef1268d9944568c3dff9
Elizabeth Gaspar <eg222@afsconnect2.njit.edu>
2017-11-08 15:09:14 -0500

    first commit

---
+++ pj1signup.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="signuphwstyle.css">
+
+<title>Welcome!</title>
+
+</head>
+
+<body bgcolor="#cc99ff">
+  
+<h1 align="center">Welcome to the sign up page!</h1>
+
+<p align="center">Please fill out the page bellow</p>
+
+<div align="center">
+    <form ><br>
+    <input type="text" id="fname" placeholder="First Name" required><br><br>
+
+    <input type="text" id="lname" placeholder="Last Name" required><br><br>
+
+    <input type="text" id="email" placeholder="Email Address" required><br><br>
+
+    <input type="text" id="phone" placeholder="Phone Number" required><br><br>
+
+    <input type="text" id="birthday" placeholder="Birthday" required><br><br>
+
+    <input type="text" id="gender" placeholder="Gender" required><br><br>
+
+  <!-- <input align="center" type="submit" value="Sign Up"/> -->
+  <button onclick="myFunction()">Sign Up</button></a>
+  </form>
+    
+</div>
+
+</body>
+</html> 
