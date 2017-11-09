5da4d83bfd225611684f2ef9b292078cbfa7dab9
elizabethmg <31739691+elizabethmg@users.noreply.github.com>
2017-11-08 22:26:38 -0500

    Update pj1login.php

--- pj1login.php
+++ pj1login.php
@@ -1,22 +1,25 @@
+?>
+
 <html>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="signuphwstyle.css">
 	<title>Login</title>
 </head>
-
-<body>
- <h1 align="center">Welcome to the sign up page!</h1>
-
-<p align="center">Please fill out the page bellow</p>
-
-Use your email to log in <?php echo $_POST["email"]; ?><br>
-
 <div align="center">
 
+<body>
+	<img src="hello.gif" alt="hello gif" style="width:300px;height:175px">
+	<body bgcolor="#99ffcc">
+	<br>
+ <h1 style="font-family:Lucida Console" align="center">Welcome to the Log In page!</h1><br>
+
+<p  style="font-family:Lucida Console" align="center">Please fill out the page bellow</p><br>
+
+
     <form action="pj1sql.php" method="POST">
-	Email: <input type="text" name="email" required><br>
-	Password: <input type="text" name="password" required><br>
+	Email: <input type="text" name="email" required><br><br>
+	Password: <input type="text" name="password" required><br><br>
 	<input type="submit">
 </form>
 </div> 
